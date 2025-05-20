571ff3c4a1617ce68d6cb10de2de3811f1351862
felipenovella <49439231@est.ort.edu.ar>
2025-05-20 09:02:50 -0300

    EjercicioTerminado

---
+++ Assets/RespawmOnFall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RespawmOnFall : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+     
+    
+    void Update()
+    {
+        if (transform.position.y < -25)
+        {
+            Debug.Log("Perdiste xd");
+            SceneManager.LoadScene("FPSparkour");
+        }
+    }
+}
