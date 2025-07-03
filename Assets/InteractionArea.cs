ca3d07793d3007664d27a548a2f1ca8c9e046317
felipenovella <49439231@est.ort.edu.ar>
2025-07-03 12:07:12 -0300

    clase 3/7

---
+++ Assets/InteractionArea.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionArea : MonoBehaviour
+{
+  
+    public GameObject interactionMessage;
+    public mercancias mercancia;
+    // Start is called before the first frame update
+    void Start()
+    {
+        interactionMessage.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.name);
+        mercancia = other.GetComponent<mercancias>();
+        if (mercancia)
+        {
+            interactionMessage.SetActive(true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        
+        interactionMessage.SetActive(false);
+    }
+}
